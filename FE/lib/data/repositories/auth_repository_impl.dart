f48a8bcdfabb2f2da477515fca03de97f5e2f9fa
jinwooseok <jinus7949@naver.com>
2025-03-18 16:31:19 +0900

    feat: 데이터 레이어 구현

---
+++ FE/lib/data/repositories/auth_repository_impl.dart
@@ -0,0 +1,21 @@
+import '../../domain/repositories/auth_repository.dart';
+import '../datasources/auth_remote_data_source.dart';
+import '../models/user_model.dart';
+
+/// 데이터를 받아오는 data부분에서 데이터소스를 동작시켜 데이터를 받아온다.
+class AuthRepositoryImpl implements AuthRepository {
+  final AuthRemoteDataSource remoteDataSource;
+
+  AuthRepositoryImpl(this.remoteDataSource);
+
+  @override
+  Future<void> signUp(String email, String nickname, String password) async {
+    final userModel = UserModel(
+      email: email,
+      nickname: nickname,
+      password: password,
+    );
+    
+    return await remoteDataSource.signUp(userModel);
+  }
+}
