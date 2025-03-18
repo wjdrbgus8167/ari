1f13532b892f66681c421c4a51492e16d7428c17
권남희 <knaem.edu@gmail.com>
2025-03-19 00:57:09 +0900

    style: 하단 네비게이션 바 디자인 수정

--- FE/lib/presentation/widgets/common/bottom_nav.dart
+++ FE/lib/presentation/widgets/common/bottom_nav.dart
@@ -5,10 +5,10 @@ class CommonBottomNav extends StatelessWidget {
   final ValueChanged<int> onTap;
 
   const CommonBottomNav({
-    Key? key,
+    super.key,
     required this.currentIndex,
     required this.onTap,
-  }) : super(key: key);
+  });
 
   @override
   Widget build(BuildContext context) {
@@ -27,13 +27,32 @@ class CommonBottomNav extends StatelessWidget {
         showUnselectedLabels: true,
         selectedFontSize: 12,
         unselectedFontSize: 12,
+        elevation: 0,
         onTap: onTap,
         items: const [
-          BottomNavigationBarItem(icon: Icon(Icons.home), label: '홈'),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home_outlined),
+            activeIcon: Icon(Icons.home),
+            label: '홈',
+          ),
           BottomNavigationBarItem(icon: Icon(Icons.search), label: '검색'),
-          BottomNavigationBarItem(icon: Icon(Icons.menu), label: '서랍'),
-          BottomNavigationBarItem(icon: Icon(Icons.person), label: '마이페이지'),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.library_music_outlined),
+            activeIcon: Icon(Icons.library_music),
+            label: '음악 서랍',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.account_circle_outlined),
+            activeIcon: Icon(Icons.account_circle),
+            label: '나의 채널',
+          ),
         ],
+        // items: const [
+        //   BottomNavigationBarItem(icon: Icon(Icons.home), label: '홈'),
+        //   BottomNavigationBarItem(icon: Icon(Icons.search), label: '검색'),
+        //   BottomNavigationBarItem(icon: Icon(Icons.menu), label: '서랍'),
+        //   BottomNavigationBarItem(icon: Icon(Icons.person), label: '마이페이지'),
+        // ],
       ),
     );
   }
