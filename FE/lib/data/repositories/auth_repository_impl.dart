500e14ed27ef2aef01ace28825003c87d9ce8b19
jinwooseok <jinus7949@naver.com>
2025-03-19 00:33:09 +0900

    refactor: 모델명 변경경

--- FE/lib/data/repositories/auth_repository_impl.dart
+++ FE/lib/data/repositories/auth_repository_impl.dart
@@ -1,6 +1,6 @@
 import '../../domain/repositories/auth_repository.dart';
 import '../datasources/auth_remote_data_source.dart';
-import '../models/user_model.dart';
+import '../models/sign_up_request.dart';
 
 /// 데이터를 받아오는 data부분에서 데이터소스를 동작시켜 데이터를 받아온다.
 class AuthRepositoryImpl implements AuthRepository {
@@ -10,7 +10,7 @@ class AuthRepositoryImpl implements AuthRepository {
 
   @override
   Future<void> signUp(String email, String nickname, String password) async {
-    final userModel = UserModel(
+    final userModel = SignUpRequest(
       email: email,
       nickname: nickname,
       password: password,
