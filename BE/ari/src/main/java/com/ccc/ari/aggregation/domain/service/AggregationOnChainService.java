79956bff9061b62f3976c80811c8c7c74258f62c
김준석 <crush0047@gmail.com>
2025-03-19 15:27:47 +0900

    feat: AggregationOnChainService 구현
    - publishAggregateData: AggregatedData를 IPFS에 저장하고, CID와 머클 루트를 블록체인에 커밋 및 로깅

--- BE/ari/src/main/java/com/ccc/ari/aggregation/domain/service/AggregationOnChainService.java
+++ BE/ari/src/main/java/com/ccc/ari/aggregation/domain/service/AggregationOnChainService.java
@@ -1,4 +1,45 @@
 package com.ccc.ari.aggregation.domain.service;
 
+import com.ccc.ari.aggregation.domain.AggregatedData;
+import com.ccc.ari.aggregation.domain.client.BlockChainClient;
+import com.ccc.ari.aggregation.domain.client.IpfsClient;
+import com.ccc.ari.aggregation.domain.client.IpfsResponse;
+import lombok.RequiredArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * AggregationOnChainService는 AggregatedData를 기반으로
+ * IPFS에 스트리밍 집계 데이터를 저장하고,
+ * 해당 데이터의 CID와 머클 루트를 블록체인에 커밋하는 도메인 서비스입니다.
+ */
+@RequiredArgsConstructor
 public class AggregationOnChainService {
+
+    private final IpfsClient ipfsClient;
+    private final BlockChainClient blockChainClient;
+    private static final Logger logger = LoggerFactory.getLogger(AggregationOnChainService.class);
+
+    /**
+     * AggregatedData를 직렬화하여 IPFS에 저장하고,
+     * 반환된 CID와 머클 루트를 블록체인에 커밋합니다.
+     *
+     * @param aggregatedData 집계 결과 Aggregate Root
+     * @return 트랜잭션 해시
+     */
+    public String publishAggregatedData(AggregatedData aggregatedData) {
+        // 1. AggregatedData를 직렬화
+        String jsonData = aggregatedData.toJson();
+        logger.info("AggregatedData JSON 직렬화: {}", jsonData);
+
+        // 2. IPFS에 데이터를 저장하고, CID와 머클 루트를 포함하는 IpfsResponse를 획득
+        IpfsResponse ipfsResponse = ipfsClient.save(jsonData);
+        logger.info("IPFS 데이터 저장 완료 CID: {} Merkle Root: {}", ipfsResponse.getCid(), ipfsResponse.getMerkleRoot());
+
+        // 3. 획득한 CID와 머클 루트를 블록체인에 커밋
+        String txHash = blockChainClient.commitAggregatedData(ipfsResponse.getCid(), ipfsResponse.getMerkleRoot());
+        logger.info("CID, Merkle Root Blockchain에 커밋 완료 Transaction Hash: {}", txHash);
+
+        return txHash;
+    }
 }
