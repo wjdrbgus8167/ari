cf96c98edca6c9444f483460e17e4d543c7f98ad
강지민 <kjmemail2743@gmail.com>
2025-03-21 02:53:22 +0900

    hotfix: SecurityConfig CORS 설정 수정

--- BE/ari/src/main/java/com/ccc/ari/global/config/SecurityConfig.java
+++ BE/ari/src/main/java/com/ccc/ari/global/config/SecurityConfig.java
@@ -29,7 +29,7 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(Arrays.asList("*")); // 모든 출처 허용 (추후 수정!!)
+        configuration.setAllowedOriginPatterns(Arrays.asList("*")); // 모든 출처 허용 (추후 수정!!)
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "PATCH", "DELETE"));
         configuration.setAllowedHeaders(Arrays.asList("*"));
         configuration.setAllowCredentials(true);
