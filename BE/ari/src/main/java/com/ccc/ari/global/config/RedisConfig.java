949a6a00f358ce704b01b31e3e094de764891ff0
김준석 <crush0047@gmail.com>
2025-03-21 02:56:45 +0900

    feat: RedisConfig 클래스 추가

---
+++ BE/ari/src/main/java/com/ccc/ari/global/config/RedisConfig.java
@@ -0,0 +1,42 @@
+package com.ccc.ari.global.config;
+
+import com.ccc.ari.aggregation.domain.vo.StreamingLog;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.repository.configuration.EnableRedisRepositories;
+import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+
+@Configuration
+@EnableRedisRepositories
+public class RedisConfig {
+
+    @Value("${REDIS_HOST}")
+    private String redisHost;
+
+    private int redisPort = 6379;
+
+    @Bean
+    public RedisConnectionFactory redisConnectionFactory() {
+        RedisStandaloneConfiguration redisConfig = new RedisStandaloneConfiguration(redisHost, redisPort);
+        return new LettuceConnectionFactory(redisConfig);
+    }
+
+    @Bean
+    public RedisTemplate<String, Object> redisTemplate() {
+        RedisTemplate<String, Object> template = new RedisTemplate<>();
+        template.setConnectionFactory(redisConnectionFactory());
+        template.setKeySerializer(new StringRedisSerializer());
+
+        // Jackson2JsonRedisSerializer 사용
+        Jackson2JsonRedisSerializer<StreamingLog> serializer =
+                new Jackson2JsonRedisSerializer<>(StreamingLog.class);
+        template.setValueSerializer(new Jackson2JsonRedisSerializer<>(Object.class));
+        return template;
+    }
+}
