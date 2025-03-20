e6f13253b00be5e4803d74c9e36b36052f68a818
kyuhyeon <wjdrbgus8167@naver.com>
2025-03-21 01:52:36 +0900

    feat : SecurityConfig 추가

---
+++ BE/ari/src/main/java/com/ccc/ari/global/config/SecurityConfig.java
@@ -0,0 +1,23 @@
+package com.ccc.ari.global.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf(AbstractHttpConfigurer::disable) // CSRF 비활성화
+                .authorizeHttpRequests(auth -> auth
+                        .anyRequest().permitAll()
+                );
+
+        return http.build();
+    }
+}
+
