9c8d674901158f17207af01daec7606320e00da6
kyuhyeon <wjdrbgus8167@naver.com>
2025-03-24 16:47:24 +0900

    feat : Member Repository 구현

--- BE/ari/src/main/java/com/ccc/ari/member/infrastructure/JpaMemberRepository.java
+++ BE/ari/src/main/java/com/ccc/ari/member/infrastructure/JpaMemberRepository.java
@@ -7,7 +7,7 @@ import org.springframework.stereotype.Repository;
 import java.util.Optional;
 
 @Repository
-public interface JpaMemberRepository  extends JpaRepository<MemberEntity, Integer> {
+public interface JpaMemberRepository  extends JpaRepository<MemberEntity, Integer>{
     Optional<MemberEntity> findByEmail(String email);
     Optional<MemberEntity> findByMemberId(Integer memberId);
 }
