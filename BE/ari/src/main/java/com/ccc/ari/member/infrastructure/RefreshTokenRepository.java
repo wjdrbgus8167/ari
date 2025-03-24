08a8bf45dca074e4d866cc78364a4fadb8ae74f0
kyuhyeon <wjdrbgus8167@naver.com>
2025-03-24 15:23:07 +0900

    feat : RefreshToken Repository 구현

---
+++ BE/ari/src/main/java/com/ccc/ari/member/infrastructure/RefreshTokenRepository.java
@@ -0,0 +1,11 @@
+package com.ccc.ari.member.infrastructure;
+
+import com.ccc.ari.member.domain.refreshToken.RefreshTokenEntity;
+import org.springframework.data.repository.CrudRepository;
+
+import java.util.Optional;
+
+public interface RefreshTokenRepository extends CrudRepository<RefreshTokenEntity, Long> {
+
+    Optional<RefreshTokenEntity> findByEmail(String email);
+}
