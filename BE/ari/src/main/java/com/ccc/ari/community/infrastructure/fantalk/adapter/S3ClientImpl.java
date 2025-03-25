db1011c2cfb3cae6698ea38c0d7773e6602d2bea
강지민 <kjmemail2743@gmail.com>
2025-03-25 10:23:58 +0900

    feat: 이미지 업로드를 위한 S3 클라이언트 구현체 추가

---
+++ BE/ari/src/main/java/com/ccc/ari/community/infrastructure/fantalk/adapter/S3ClientImpl.java
@@ -0,0 +1,46 @@
+package com.ccc.ari.community.infrastructure.fantalk.adapter;
+
+import com.ccc.ari.community.domain.fantalk.client.S3Client;
+import com.ccc.ari.global.infrastructure.S3Service;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.util.UUID;
+
+@Component
+@RequiredArgsConstructor
+public class S3ClientImpl implements S3Client {
+
+    private final S3Service s3Service;
+
+    /**
+     * 팬톡 이미지 파일 경로 생성
+     */
+    @Override
+    public String uploadImage(MultipartFile imageFile, String directory) {
+        if (imageFile == null || imageFile.isEmpty()) {
+            return null;
+        }
+
+        String imageFilePath = generateImagePath(imageFile, directory);
+        return s3Service.uploadFileToS3(imageFile, imageFilePath);
+    }
+
+    /**
+     * 팬톡 이미지 파일 경로 생성
+     */
+    private String generateImagePath(MultipartFile file, String directory) {
+        return directory + "/images/" + UUID.randomUUID() + getFileExtension(file.getOriginalFilename());
+    }
+
+    /**
+     * 파일 확장자 추출
+     */
+    private String getFileExtension(String filename) {
+        if (filename == null || !filename.contains(".")) {
+            return ""; // 확장자가 없을 경우 빈 문자열 반환
+        }
+        return filename.substring(filename.lastIndexOf(".")); // ".jpg", ".png" 등 형태로 반환
+    }
+}
