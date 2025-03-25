cfdb62b8b9a2804568e033d10cb939136e4b3329
강지민 <kjmemail2743@gmail.com>
2025-03-25 14:03:36 +0900

    feat: 팬톡 목록 조회를 위한 리포지토리 메서드 추가

--- BE/ari/src/main/java/com/ccc/ari/community/infrastructure/fantalk/repository/FantalkJpaRepository.java
+++ BE/ari/src/main/java/com/ccc/ari/community/infrastructure/fantalk/repository/FantalkJpaRepository.java
@@ -4,9 +4,13 @@ import com.ccc.ari.community.infrastructure.fantalk.entity.FantalkJpaEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 /**
  * 팬톡 JPA 리포지토리 인터페이스
  */
 @Repository
 public interface FantalkJpaRepository extends JpaRepository<FantalkJpaEntity, Integer> {
+    // 채널 ID로 팬톡 목록 조회 (생성 일시 기준 내림차순 정렬)
+    List<FantalkJpaEntity> findByFantalkChannelIdOrderByCreatedAtDesc(Integer fantalkChannelId);
 }
