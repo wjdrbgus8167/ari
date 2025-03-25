5539ed276192c793152ed5fd8d4fb17815178103
강지민 <kjmemail2743@gmail.com>
2025-03-25 10:26:46 +0900

    feat: 팬톡 생성 Request 추가

--- BE/ari/src/main/java/com/ccc/ari/community/ui/fantalk/request/CreateFantalkRequest.java
+++ BE/ari/src/main/java/com/ccc/ari/community/ui/fantalk/request/CreateFantalkRequest.java
@@ -3,6 +3,7 @@ package com.ccc.ari.community.ui.fantalk.request;
 import com.ccc.ari.community.application.fantalk.command.CreateFantalkCommand;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.springframework.web.multipart.MultipartFile;
 
 @Getter
 @NoArgsConstructor
@@ -10,18 +11,17 @@ public class CreateFantalkRequest {
 
     private String content;
     private Integer trackId;
-    private String fantalkImageUrl;
 
     /**
      * Request -> Command 변환 메서드
      */
-    public CreateFantalkCommand toCommand(Integer memberId, Integer fantalkChannelId) {
+    public CreateFantalkCommand toCommand(Integer memberId, Integer fantalkChannelId, MultipartFile fantalkImage) {
         return CreateFantalkCommand.builder()
                 .memberId(memberId)
                 .fantalkChannelId(fantalkChannelId)
                 .content(this.content)
                 .trackId(this.trackId)
-                .fantalkImageUrl(this.fantalkImageUrl)
+                .fantalkImage(fantalkImage)
                 .build();
     }
 }
