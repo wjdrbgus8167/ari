5f681bc01307253a10678394009dd2ce9e38cbf4
kyuhyeon <wjdrbgus8167@naver.com>
2025-03-25 17:00:34 +0900

    feat : MemberController 구현

---
+++ BE/ari/src/main/java/com/ccc/ari/member/ui/MemberController.java
@@ -0,0 +1,84 @@
+package com.ccc.ari.member.ui;
+
+import com.ccc.ari.global.util.ApiUtils;
+import com.ccc.ari.global.util.CookieUtils;
+import com.ccc.ari.member.application.MemberService;
+import com.ccc.ari.member.application.command.MemberLoginCommand;
+import com.ccc.ari.member.application.command.MemberRegisterCommand;
+import com.ccc.ari.member.application.command.RefreshAccessTokenCommand;
+import com.ccc.ari.member.domain.AuthTokens;
+import com.ccc.ari.member.ui.reuquest.MemberLoginRequest;
+import com.ccc.ari.member.ui.reuquest.MemberRegisterRequest;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequiredArgsConstructor
+@RestController
+@RequestMapping("/api/v1/auth")
+public class MemberController {
+
+    private final MemberService memberService;
+
+    /**
+     * 일반 사용자 이메일 회원가입
+     */
+    @PostMapping("/members/register")
+
+    public ApiUtils.ApiResponse<?> memberRegister(@RequestBody @Valid MemberRegisterRequest request){
+
+        MemberRegisterCommand command = MemberRegisterCommand.builder()
+                .email(request.getEmail())
+                .password(request.getPassword())
+                .nickname(request.getNickname())
+                .build();
+
+        memberService.memberRegister(command);
+
+        return ApiUtils.success("회원가입이 완료되었습니다.");
+
+    }
+
+    /**
+     *  일반 사용자 이메일 로그인
+     */
+    @PostMapping("/members/login")
+    public ApiUtils.ApiResponse<?> memberLogin(
+            @RequestBody @Valid MemberLoginRequest request,
+            HttpServletResponse response
+    ) {
+
+        MemberLoginCommand command = MemberLoginCommand.builder()
+                .email(request.getEmail())
+                .password(request.getPassword())
+                .build();
+
+        AuthTokens tokens = memberService.memberLogin(command);
+        CookieUtils.addAccessTokenCookie(response, tokens.accessToken());
+        CookieUtils.addRefreshTokenCookie(response, tokens.refreshToken());
+
+        return ApiUtils.success("로그인이 완료되었습니다.");
+
+    }
+
+
+    @PostMapping("/token/refresh")
+    public ApiUtils.ApiResponse<?> refreshAccessToken(
+            HttpServletRequest request,
+            HttpServletResponse response
+    )
+    {
+        RefreshAccessTokenCommand command = RefreshAccessTokenCommand.builder()
+                .refreshToken(CookieUtils.getRefreshToken(request))
+                .build();
+
+        CookieUtils.addAccessTokenCookie(response, memberService.refreshAccessToken(command));
+        return ApiUtils.success("토큰이 재발급 되었습니다.");
+    }
+
+}
