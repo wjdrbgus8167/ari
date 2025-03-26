ff30c7535f920c82cdc3a23460ff178ef4ecccdd
강지민 <kjmemail2743@gmail.com>
2025-03-26 14:46:48 +0900

    feat: 애플리케이션 서비스에 조회 메서드 추가

--- BE/ari/src/main/java/com/ccc/ari/community/application/notice/service/NoticeService.java
+++ BE/ari/src/main/java/com/ccc/ari/community/application/notice/service/NoticeService.java
@@ -1,15 +1,19 @@
 package com.ccc.ari.community.application.notice.service;
 
 import com.ccc.ari.community.application.notice.command.CreateNoticeCommand;
+import com.ccc.ari.community.application.notice.dto.NoticeListResponseDto;
+import com.ccc.ari.community.application.notice.dto.NoticeResponseDto;
 import com.ccc.ari.community.application.notice.repository.NoticeRepository;
 import com.ccc.ari.community.domain.notice.entity.Notice;
 import com.ccc.ari.community.domain.notice.vo.NoticeContent;
 import com.ccc.ari.global.infrastructure.S3Client;
+import jakarta.persistence.EntityNotFoundException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.time.LocalDateTime;
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
@@ -41,5 +45,35 @@ public class NoticeService {
 
         // 4. 공지사항을 저장합니다.
         noticeRepository.saveNotice(notice);
+    }
+
+    @Transactional(readOnly = true)
+    public NoticeListResponseDto getNoticeList(Integer artistId) {
+        // 1. 공지사항 목록을 조회합니다.
+        List<Notice> notices = noticeRepository.findAll(artistId);
+
+        // 2. 공지사항 개수를 조회합니다.
+        int count = noticeRepository.countByArtistId(artistId);
+
+        // 3. 도메인 엔티티를 DTO로 변환합니다.
+        List<NoticeResponseDto> noticeResponseDtos = notices.stream()
+                .map(NoticeResponseDto::from)
+                .toList();
+
+        // 4. 응답 DTO를 생성하여 반환합니다.
+        return NoticeListResponseDto.builder()
+                .notices(noticeResponseDtos)
+                .noticeCount(count)
+                .build();
+    }
+
+    @Transactional(readOnly = true)
+    public NoticeResponseDto getNotice(Integer noticeId) {
+        // 1. 공지사항을 상세 조회합니다.
+        Notice notice = noticeRepository.findById(noticeId)
+                .orElseThrow(() -> new EntityNotFoundException("해당 공지사항이 없습니다. ID: " + noticeId));
+
+        // 2. 도메인 엔티티를 DTO로 변환하여 반환합니다.
+        return NoticeResponseDto.from(notice);
     }
 }
