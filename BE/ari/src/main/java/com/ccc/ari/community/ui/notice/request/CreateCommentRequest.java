34e153e62e2899bc3ebab638dd206a9a160e5881
강지민 <kjmemail2743@gmail.com>
2025-03-27 00:08:08 +0900

    feat: 공지사항 댓글 생성 Request 구현

---
+++ BE/ari/src/main/java/com/ccc/ari/community/ui/notice/request/CreateCommentRequest.java
@@ -0,0 +1,10 @@
+package com.ccc.ari.community.ui.notice.request;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+public class CreateCommentRequest {
+    private String content;
+}
