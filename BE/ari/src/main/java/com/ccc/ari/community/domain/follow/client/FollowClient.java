f3ded3e6d5a4933e3e713b752c3d60fd8af6ae44
강지민 <kjmemail2743@gmail.com>
2025-03-27 20:26:29 +0900

    feat: 팔로우 도메인 접근을 위한 Client 인터페이스와 구현체

--- BE/ari/src/main/java/com/ccc/ari/community/domain/follow/client/FollowClient.java
+++ BE/ari/src/main/java/com/ccc/ari/community/domain/follow/client/FollowClient.java
@@ -1,8 +1,24 @@
 package com.ccc.ari.community.domain.follow.client;
 
+import java.util.List;
+import java.util.Map;
+
 /**
  * 팔로우 도메인에 접근하기 위한 클라이언트 인터페이스
  */
 public interface FollowClient {
-    // TODO: 팔로잉 목록, 팔로우 목록, 팔로잉 수, 팔로워 수 조회
+    // 팔로잉 목록 조회
+    List<FollowingDto> getFollowingsByMemberId(Integer memberId);
+
+    // 팔로워 목록 조회
+    List<FollowerDto> getFollowersByMemberId(Integer memberId);
+
+    // 팔로잉 수 조회
+    int countFollowingsByMemberId(Integer memberId);
+
+    // 팔로워 수 조회
+    int countFollowersByMemberId(Integer memberId);
+
+    // 여러 회원의 팔로워 수를 한 번에 조회하는 메서드 추가
+    Map<Integer, Integer> countFollowersByMemberIds(List<Integer> memberIds);
 }
