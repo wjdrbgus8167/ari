6f282b196a8ef7518be05253cac146a640418374
권남희 <knaem.edu@gmail.com>
2025-03-28 13:52:43 +0900

    refactor: 나의 채널 관련 provider 작성 및 파일 이동

--- FE/lib/providers/global_providers.dart
+++ FE/lib/providers/global_providers.dart
@@ -7,19 +7,13 @@ import 'package:audioplayers/audioplayers.dart';
 
 import 'package:ari/presentation/viewmodels/home_viewmodel.dart';
 import 'package:ari/presentation/viewmodels/listening_queue_viewmodel.dart';
-import 'package:ari/presentation/viewmodels/my_channel_viewmodel.dart';
 
 import 'package:ari/data/models/track.dart';
 import 'package:ari/data/repositories/chart_repository_impl.dart';
 import 'package:ari/data/datasources/chart_remote_data_source.dart';
-import 'package:ari/data/datasources/my_channel/my_channel_remote_datasource.dart';
-import 'package:ari/data/datasources/my_channel/my_channel_remote_datasource_impl.dart';
-import 'package:ari/data/repositories/my_channel/my_channel_repository_impl.dart';
 
 import 'package:ari/domain/repositories/chart_repository.dart';
-import 'package:ari/domain/repositories/my_channel/my_channel_repository.dart';
 import 'package:ari/domain/usecases/get_charts_usecase.dart';
-import 'package:ari/domain/usecases/my_channel/my_channel_usecases.dart';
 
 import 'package:ari/core/services/playback_service.dart';
 import 'package:ari/core/constants/app_constants.dart';
@@ -145,79 +139,3 @@ final listeningQueueProvider =
     StateNotifierProvider<ListeningQueueViewModel, ListeningQueueState>(
       (ref) => ListeningQueueViewModel(),
     );
-
-// ========== 나의 채널 관련 Provider 추가 ==========
-
-/// 나의 채널 원격 데이터 소스 제공자
-final myChannelRemoteDataSourceProvider = Provider<MyChannelRemoteDataSource>((
-  ref,
-) {
-  final dio = ref.watch(dioProvider);
-  final token = ref.watch(authTokenProvider);
-
-  return MyChannelRemoteDataSourceImpl(dio: dio, token: token);
-});
-
-/// 나의 채널 리포지토리 제공자
-final myChannelRepositoryProvider = Provider<MyChannelRepository>((ref) {
-  final remoteDataSource = ref.watch(myChannelRemoteDataSourceProvider);
-
-  return MyChannelRepositoryImpl(remoteDataSource: remoteDataSource);
-});
-
-/// UseCase 제공자들
-final getChannelInfoUseCaseProvider = Provider<GetChannelInfoUseCase>((ref) {
-  return GetChannelInfoUseCase(ref.watch(myChannelRepositoryProvider));
-});
-
-final followMemberUseCaseProvider = Provider<FollowMemberUseCase>((ref) {
-  return FollowMemberUseCase(ref.watch(myChannelRepositoryProvider));
-});
-
-final unfollowMemberUseCaseProvider = Provider<UnfollowMemberUseCase>((ref) {
-  return UnfollowMemberUseCase(ref.watch(myChannelRepositoryProvider));
-});
-
-final getArtistAlbumsUseCaseProvider = Provider<GetArtistAlbumsUseCase>((ref) {
-  return GetArtistAlbumsUseCase(ref.watch(myChannelRepositoryProvider));
-});
-
-final getArtistNoticesUseCaseProvider = Provider<GetArtistNoticesUseCase>((
-  ref,
-) {
-  return GetArtistNoticesUseCase(ref.watch(myChannelRepositoryProvider));
-});
-
-final getFanTalksUseCaseProvider = Provider<GetFanTalksUseCase>((ref) {
-  return GetFanTalksUseCase(ref.watch(myChannelRepositoryProvider));
-});
-
-final getPublicPlaylistsUseCaseProvider = Provider<GetPublicPlaylistsUseCase>((
-  ref,
-) {
-  return GetPublicPlaylistsUseCase(ref.watch(myChannelRepositoryProvider));
-});
-
-final getFollowersUseCaseProvider = Provider<GetFollowersUseCase>((ref) {
-  return GetFollowersUseCase(ref.watch(myChannelRepositoryProvider));
-});
-
-final getFollowingsUseCaseProvider = Provider<GetFollowingsUseCase>((ref) {
-  return GetFollowingsUseCase(ref.watch(myChannelRepositoryProvider));
-});
-
-/// 나의 채널 뷰모델 제공자
-final myChannelProvider =
-    StateNotifierProvider<MyChannelNotifier, MyChannelState>((ref) {
-      return MyChannelNotifier(
-        getChannelInfoUseCase: ref.watch(getChannelInfoUseCaseProvider),
-        followMemberUseCase: ref.watch(followMemberUseCaseProvider),
-        unfollowMemberUseCase: ref.watch(unfollowMemberUseCaseProvider),
-        getArtistAlbumsUseCase: ref.watch(getArtistAlbumsUseCaseProvider),
-        getArtistNoticesUseCase: ref.watch(getArtistNoticesUseCaseProvider),
-        getFanTalksUseCase: ref.watch(getFanTalksUseCaseProvider),
-        getPublicPlaylistsUseCase: ref.watch(getPublicPlaylistsUseCaseProvider),
-        getFollowersUseCase: ref.watch(getFollowersUseCaseProvider),
-        getFollowingsUseCase: ref.watch(getFollowingsUseCaseProvider),
-      );
-    });
--- FE/lib/providers/my_channel_providers.dart
+++ FE/lib/providers/my_channel_providers.dart
@@ -1,10 +1,26 @@
-import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:dio/dio.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import '../core/constants/app_constants.dart';
+import '../providers/auth/auth_providers.dart'; // 인증 관련 provider
+import '../providers/global_providers.dart'; // dioProvider 등 전역 provider 참조용
+
+// datasource
 import '../data/datasources/my_channel/my_channel_remote_datasource.dart';
 import '../data/datasources/my_channel/my_channel_remote_datasource_impl.dart';
+import '../data/datasources/my_channel/artist_notice_remote_datasource.dart';
+import '../data/datasources/my_channel/artist_notice_remote_datasource_impl.dart';
+
+// repository
 import '../data/repositories/my_channel/my_channel_repository_impl.dart';
+import '../data/repositories/my_channel/artist_notice_repository_impl.dart';
 import '../domain/repositories/my_channel/my_channel_repository.dart';
-import '../domain/usecases/my_channel/my_channel_usecases.dart';
+import '../domain/repositories/my_channel/artist_notice_repository.dart';
+
+// usecase
+import '../domain/usecases/my_channel/my_channel_usecases.dart' as channel;
+import '../domain/usecases/my_channel/artist_notice_usecases.dart' as notice;
+
+// viewmodel
 import '../presentation/viewmodels/my_channel_viewmodel.dart';
 
 /// TODO: 토큰 provider - 추후 Secure Storage에서 토큰을 가져올 예정!!!!!!!
@@ -19,73 +35,131 @@ final dioProvider = Provider<Dio>((ref) {
 });
 
 /// 나의 채널 원격 데이터 소스 provider
-final myChannelRemoteDataSourceProvider = Provider<MyChannelRemoteDataSource>((ref) {
+final myChannelRemoteDataSourceProvider = Provider<MyChannelRemoteDataSource>((
+  ref,
+) {
   final dio = ref.watch(dioProvider);
   final token = ref.watch(authTokenProvider);
-  
-  return MyChannelRemoteDataSourceImpl(
-    dio: dio,
-    token: token,
-  );
+
+  return MyChannelRemoteDataSourceImpl(dio: dio, token: token);
 });
 
 /// 나의 채널 repository provider
 final myChannelRepositoryProvider = Provider<MyChannelRepository>((ref) {
   final remoteDataSource = ref.watch(myChannelRemoteDataSourceProvider);
-  
-  return MyChannelRepositoryImpl(
-    remoteDataSource: remoteDataSource,
-  );
+
+  return MyChannelRepositoryImpl(remoteDataSource: remoteDataSource);
 });
 
 /// UseCase provider
-final getChannelInfoUseCaseProvider = Provider<GetChannelInfoUseCase>((ref) {
-  return GetChannelInfoUseCase(ref.watch(myChannelRepositoryProvider));
+final getChannelInfoUseCaseProvider = Provider<channel.GetChannelInfoUseCase>((
+  ref,
+) {
+  return channel.GetChannelInfoUseCase(ref.watch(myChannelRepositoryProvider));
 });
 
-final followMemberUseCaseProvider = Provider<FollowMemberUseCase>((ref) {
-  return FollowMemberUseCase(ref.watch(myChannelRepositoryProvider));
+final followMemberUseCaseProvider = Provider<channel.FollowMemberUseCase>((
+  ref,
+) {
+  return channel.FollowMemberUseCase(ref.watch(myChannelRepositoryProvider));
 });
 
-final unfollowMemberUseCaseProvider = Provider<UnfollowMemberUseCase>((ref) {
-  return UnfollowMemberUseCase(ref.watch(myChannelRepositoryProvider));
+final unfollowMemberUseCaseProvider = Provider<channel.UnfollowMemberUseCase>((
+  ref,
+) {
+  return channel.UnfollowMemberUseCase(ref.watch(myChannelRepositoryProvider));
 });
 
-final getArtistAlbumsUseCaseProvider = Provider<GetArtistAlbumsUseCase>((ref) {
-  return GetArtistAlbumsUseCase(ref.watch(myChannelRepositoryProvider));
+final getArtistAlbumsUseCaseProvider = Provider<channel.GetArtistAlbumsUseCase>(
+  (ref) {
+    return channel.GetArtistAlbumsUseCase(
+      ref.watch(myChannelRepositoryProvider),
+    );
+  },
+);
+
+// 채널용 공지사항 provider
+final getChannelRecentNoticesUseCaseProvider =
+    Provider<channel.GetChannelRecentNoticesUseCase>((ref) {
+      return channel.GetChannelRecentNoticesUseCase(
+        ref.watch(myChannelRepositoryProvider),
+      );
+    });
+
+final getFanTalksUseCaseProvider = Provider<channel.GetFanTalksUseCase>((ref) {
+  return channel.GetFanTalksUseCase(ref.watch(myChannelRepositoryProvider));
 });
 
-final getArtistNoticesUseCaseProvider = Provider<GetArtistNoticesUseCase>((ref) {
-  return GetArtistNoticesUseCase(ref.watch(myChannelRepositoryProvider));
+final getPublicPlaylistsUseCaseProvider =
+    Provider<channel.GetPublicPlaylistsUseCase>((ref) {
+      return channel.GetPublicPlaylistsUseCase(
+        ref.watch(myChannelRepositoryProvider),
+      );
+    });
+
+final getFollowersUseCaseProvider = Provider<channel.GetFollowersUseCase>((
+  ref,
+) {
+  return channel.GetFollowersUseCase(ref.watch(myChannelRepositoryProvider));
 });
 
-final getFanTalksUseCaseProvider = Provider<GetFanTalksUseCase>((ref) {
-  return GetFanTalksUseCase(ref.watch(myChannelRepositoryProvider));
+final getFollowingsUseCaseProvider = Provider<channel.GetFollowingsUseCase>((
+  ref,
+) {
+  return channel.GetFollowingsUseCase(ref.watch(myChannelRepositoryProvider));
 });
 
-final getPublicPlaylistsUseCaseProvider = Provider<GetPublicPlaylistsUseCase>((ref) {
-  return GetPublicPlaylistsUseCase(ref.watch(myChannelRepositoryProvider));
-});
+/// 아티스트 공지사항 전용 provider
+final getArtistNoticesUseCaseProvider =
+    Provider<notice.GetArtistNoticesUseCase>((ref) {
+      final repository = ref.watch(artistNoticeRepositoryProvider);
+      return notice.GetArtistNoticesUseCase(repository);
+    });
 
-final getFollowersUseCaseProvider = Provider<GetFollowersUseCase>((ref) {
-  return GetFollowersUseCase(ref.watch(myChannelRepositoryProvider));
-});
+final getArtistNoticeDetailUseCaseProvider =
+    Provider<notice.GetArtistNoticeDetailUseCase>((ref) {
+      final repository = ref.watch(artistNoticeRepositoryProvider);
+      return notice.GetArtistNoticeDetailUseCase(repository);
+    });
 
-final getFollowingsUseCaseProvider = Provider<GetFollowingsUseCase>((ref) {
-  return GetFollowingsUseCase(ref.watch(myChannelRepositoryProvider));
+final createArtistNoticeUseCaseProvider =
+    Provider<notice.CreateArtistNoticeUseCase>((ref) {
+      final repository = ref.watch(artistNoticeRepositoryProvider);
+      return notice.CreateArtistNoticeUseCase(repository);
+    });
+
+// 아티스트 공지사항 데이터소스 provider
+final artistNoticeRemoteDataSourceProvider =
+    Provider<ArtistNoticeRemoteDataSource>((ref) {
+      final dio = ref.watch(dioProvider);
+      return ArtistNoticeRemoteDataSourceImpl(dio: dio);
+    });
+
+// 아티스트 공지사항 repository provider
+final artistNoticeRepositoryProvider = Provider<ArtistNoticeRepository>((ref) {
+  final remoteDataSource = ref.watch(artistNoticeRemoteDataSourceProvider);
+  return ArtistNoticeRepositoryImpl(remoteDataSource: remoteDataSource);
 });
 
 /// 나의 채널 ViewModel provider
-final myChannelProvider = StateNotifierProvider<MyChannelNotifier, MyChannelState>((ref) {
-  return MyChannelNotifier(
-    getChannelInfoUseCase: ref.watch(getChannelInfoUseCaseProvider),
-    followMemberUseCase: ref.watch(followMemberUseCaseProvider),
-    unfollowMemberUseCase: ref.watch(unfollowMemberUseCaseProvider),
-    getArtistAlbumsUseCase: ref.watch(getArtistAlbumsUseCaseProvider),
-    getArtistNoticesUseCase: ref.watch(getArtistNoticesUseCaseProvider),
-    getFanTalksUseCase: ref.watch(getFanTalksUseCaseProvider),
-    getPublicPlaylistsUseCase: ref.watch(getPublicPlaylistsUseCaseProvider),
-    getFollowersUseCase: ref.watch(getFollowersUseCaseProvider),
-    getFollowingsUseCase: ref.watch(getFollowingsUseCaseProvider),
-  );
-});
+final myChannelProvider =
+    StateNotifierProvider<MyChannelNotifier, MyChannelState>((ref) {
+      return MyChannelNotifier(
+        getChannelInfoUseCase: ref.watch(getChannelInfoUseCaseProvider),
+        followMemberUseCase: ref.watch(followMemberUseCaseProvider),
+        unfollowMemberUseCase: ref.watch(unfollowMemberUseCaseProvider),
+        getArtistAlbumsUseCase: ref.watch(getArtistAlbumsUseCaseProvider),
+        getArtistNoticesUseCase: ref.watch(
+          getChannelRecentNoticesUseCaseProvider,
+        ),
+        getFanTalksUseCase: ref.watch(getFanTalksUseCaseProvider),
+        getPublicPlaylistsUseCase: ref.watch(getPublicPlaylistsUseCaseProvider),
+        getFollowersUseCase: ref.watch(getFollowersUseCaseProvider),
+        getFollowingsUseCase: ref.watch(getFollowingsUseCaseProvider),
+
+        getArtistNoticeDetailUseCase: ref.watch(
+          getArtistNoticeDetailUseCaseProvider,
+        ),
+        createArtistNoticeUseCase: ref.watch(createArtistNoticeUseCaseProvider),
+      );
+    });
