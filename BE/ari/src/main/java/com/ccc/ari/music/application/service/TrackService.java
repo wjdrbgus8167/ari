a8ae661c7f2dba243bc63394b6c136c88ec077e7
kyuhyeon <wjdrbgus8167@naver.com>
2025-03-30 17:31:50 +0900

    feat : 앨범,트랙 Service 구현

---
+++ BE/ari/src/main/java/com/ccc/ari/music/application/service/TrackService.java
@@ -0,0 +1,29 @@
+package com.ccc.ari.music.application.service;
+
+import com.ccc.ari.global.error.ApiException;
+import com.ccc.ari.global.error.ErrorCode;
+import com.ccc.ari.music.domain.track.TrackDto;
+import com.ccc.ari.music.domain.track.TrackEntity;
+import com.ccc.ari.music.infrastructure.track.JpaTrackRepository;
+import com.ccc.ari.music.mapper.TrackMapper;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class TrackService {
+
+    private final JpaTrackRepository jpaTrackRepository;
+
+    public List<TrackDto> getTracksByAlbumId(Integer albumId){
+        List<TrackEntity> entities = jpaTrackRepository.findAllByAlbum_AlbumId(albumId)
+                .orElseThrow(() -> new ApiException(ErrorCode.MUSIC_FILE_NOT_FOUND));
+
+        return entities.stream()
+                .map(TrackMapper::toDto)
+                .toList();
+
+    }
+}
