979f73cebf1a411a84584c3fa082c87cc0a5d80b
kyuhyeon <wjdrbgus8167@naver.com>
2025-03-31 10:38:06 +0900

    feat : 인증 Validator 구현

---
+++ BE/ari/src/main/java/com/ccc/ari/member/validator/ValidPassword.java
@@ -0,0 +1,23 @@
+package com.ccc.ari.member.validator;
+
+import jakarta.validation.Constraint;
+import jakarta.validation.Payload;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target(ElementType.FIELD)
+@Retention(RetentionPolicy.RUNTIME)
+@Constraint(validatedBy = PasswordValidator.class)
+public @interface ValidPassword {
+    String message() default "비밀번호가 유효하지 않습니다.";
+
+    Class<?>[] groups() default {};
+
+    Class<? extends Payload>[] payload() default {};
+
+    int min() default 4;
+    int max() default 72; // Bcrypt가 처리 하는 최대 길이
+}
