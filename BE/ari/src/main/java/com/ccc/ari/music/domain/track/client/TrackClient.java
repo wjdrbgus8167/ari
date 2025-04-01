6062a661190397a95805ee05b95d66376a3cac59
kyuhyeon <wjdrbgus8167@naver.com>
2025-04-01 11:59:32 +0900

    feat : 메서드 추가 구현

--- BE/ari/src/main/java/com/ccc/ari/music/domain/track/client/TrackClient.java
+++ BE/ari/src/main/java/com/ccc/ari/music/domain/track/client/TrackClient.java
@@ -1,10 +1,13 @@
 package com.ccc.ari.music.domain.track.client;
 
 import com.ccc.ari.music.domain.track.TrackDto;
+import com.ccc.ari.music.domain.track.TrackEntity;
 
 import java.util.List;
 
 public interface TrackClient {
     List<TrackDto> getTracksByAlbumId(Integer albumId);
     TrackDto getTrackById(Integer trackId);
+    List<TrackEntity> saveTracks(List<TrackEntity> trackEntities);
+    TrackDto getTrackByAlbumIdAndTrackId(Integer albumId, Integer trackId);
 }
