f40c9777deba1f53a1ec40acd78ff52a7b41907d
songowen <alslvls1995@naver.com>
2025-04-03 11:40:06 +0900

    fix: 재생목록 중복된 트랙 안보이던 오류 수정

--- FE/lib/data/models/listening_queue_item.dart
+++ FE/lib/data/models/listening_queue_item.dart
@@ -1,18 +1,19 @@
 import 'package:ari/domain/entities/track.dart';
+import 'package:flutter/material.dart';
 
 class ListeningQueueItem {
   final Track track;
-  final String uniqueId;
+  final String uuid; // 고유 ID
 
-  ListeningQueueItem({required this.track})
-    : uniqueId = DateTime.now().millisecondsSinceEpoch.toString();
+  ListeningQueueItem({required this.track}) : uuid = UniqueKey().toString();
 
-  // 동일한 중복 트랙을 uniqueid로 비교하게 함
   @override
   bool operator ==(Object other) =>
       identical(this, other) ||
-      other is ListeningQueueItem && other.uniqueId == uniqueId;
+      other is ListeningQueueItem && uuid == other.uuid;
 
   @override
-  int get hashCode => uniqueId.hashCode;
+  int get hashCode => uuid.hashCode;
+
+  String get uniqueId => uuid;
 }
