90a164929708ae2d2f600bd1321da18a30d9abab
강지민 <kjmemail2743@gmail.com>
2025-04-04 15:46:09 +0900

    feat: 인기 플레이리스트 조회 실패 에러 추가

--- BE/ari/src/main/java/com/ccc/ari/global/error/ErrorCode.java
+++ BE/ari/src/main/java/com/ccc/ari/global/error/ErrorCode.java
@@ -67,6 +67,7 @@ public enum ErrorCode {
     POPULAR_GENRE_ALBUM_NOT_FOUND(HttpStatus.NOT_FOUND, "E002", "장르별 인기 앨범을 조회하는데 실패했습니다."),
     POPULAR_ALL_TRACK_NOT_FOUND(HttpStatus.NOT_FOUND, "E003", "인기 트랙을 조회하는데 실패했습니다."),
     POPULAR_GENRE_TRACK_NOT_FOUND(HttpStatus.NOT_FOUND, "E004", "장르별 인기 트랙을 조회하는데 실패했습니다."),
+    POPULAR_PLAYLIST_NOT_FOUND(HttpStatus.NOT_FOUND, "E005", "인기 플레이리스트를 조회하는데 실패했습니다."),
 
     GENRE_NOT_FOUND(HttpStatus.NOT_FOUND,"G001","존재하지 않는 장르입니다."),
 
