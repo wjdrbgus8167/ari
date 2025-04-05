54b04e4c2749e725076ce0ff10c59f03ca6be270
강지민 <kjmemail2743@gmail.com>
2025-04-05 14:55:59 +0900

    feat: 트랙, 회원 검색을 위한 Composition 컨트롤러 구현

---
+++ BE/ari/src/main/java/com/ccc/ari/global/composition/controller/search/SearchController.java
@@ -0,0 +1,24 @@
+package com.ccc.ari.global.composition.controller.search;
+
+import com.ccc.ari.global.composition.response.search.SearchResponse;
+import com.ccc.ari.global.composition.service.search.SearchService;
+import com.ccc.ari.global.util.ApiUtils;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/search")
+@RequiredArgsConstructor
+public class SearchController {
+
+    private final SearchService searchService;
+
+    @GetMapping
+    public ApiUtils.ApiResponse<SearchResponse> search(@RequestParam("q") String query) {
+        SearchResponse response = searchService.search(query);
+        return ApiUtils.success(response);
+    }
+}
