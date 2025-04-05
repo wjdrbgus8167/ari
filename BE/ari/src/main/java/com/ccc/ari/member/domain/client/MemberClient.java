f79308ce15e77f214a6f0dd20bdd9eb0a1b6d163
강지민 <kjmemail2743@gmail.com>
2025-04-05 14:40:50 +0900

    feat: 회원 검색을 위한 Client 메서드 추가

--- BE/ari/src/main/java/com/ccc/ari/member/domain/client/MemberClient.java
+++ BE/ari/src/main/java/com/ccc/ari/member/domain/client/MemberClient.java
@@ -2,8 +2,13 @@ package com.ccc.ari.member.domain.client;
 
 import com.ccc.ari.member.domain.member.MemberDto;
 
+import java.util.List;
+
 public interface MemberClient {
     // Member의 닉네임 조회
     String getNicknameByMemberId(Integer memberId);
     MemberDto  getMemberByMemberId(Integer memberId);
+
+    // 회원 검색
+    List<MemberDto> searchMembersByKeyword(String query);
 }
--- BE/ari/src/main/java/com/ccc/ari/member/infrastructure/repository/member/JpaMemberRepository.java
+++ BE/ari/src/main/java/com/ccc/ari/member/infrastructure/repository/member/JpaMemberRepository.java
@@ -4,10 +4,12 @@ import com.ccc.ari.member.domain.member.MemberEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
 public interface JpaMemberRepository  extends JpaRepository<MemberEntity, Integer>{
     Optional<MemberEntity> findByEmail(String email);
     Optional<MemberEntity> findByMemberId(Integer memberId);
+    List<MemberEntity> findByNicknameContaining(String keyword);
 }
