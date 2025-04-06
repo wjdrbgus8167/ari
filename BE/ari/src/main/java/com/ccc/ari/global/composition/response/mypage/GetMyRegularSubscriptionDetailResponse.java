4fe71d2ae5cf7658a23b1973ae93486be1769e53
kyuhyeon <wjdrbgus8167@naver.com>
2025-04-06 18:26:25 +0900

    feat: 내 정기 구독 내역 세부 조회

---
+++ BE/ari/src/main/java/com/ccc/ari/global/composition/response/mypage/GetMyRegularSubscriptionDetailResponse.java
@@ -0,0 +1,42 @@
+package com.ccc.ari.global.composition.response.mypage;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.List;
+
+@Getter
+@NoArgsConstructor
+public class GetMyRegularSubscriptionDetailResponse {
+
+    private BigDecimal price;
+    private List<Settlement> settlements;
+
+    @Builder
+    public GetMyRegularSubscriptionDetailResponse(BigDecimal price,List<Settlement> settlements) {
+        this.price = price;
+        this.settlements = new ArrayList<>();
+    }
+
+    @Getter
+    @NoArgsConstructor
+    public static class Settlement{
+        private String artistNickName;
+        private String profileImageUrl;
+        private Integer StreamingCount;
+        private BigDecimal settlement;
+
+        @Builder
+        public Settlement(String artistNickName, String profileImageUrl
+                , Integer StreamingCount, BigDecimal settlement) {
+
+            this.artistNickName = artistNickName;
+            this.profileImageUrl = profileImageUrl;
+            this.StreamingCount = StreamingCount;
+            this.settlement = settlement;
+        }
+    }
+}
