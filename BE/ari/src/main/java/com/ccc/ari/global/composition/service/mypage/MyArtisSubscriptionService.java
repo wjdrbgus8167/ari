8839dca0c8d9028a3a9b78426b96ca931050f7df
kyuhyeon <wjdrbgus8167@naver.com>
2025-04-07 00:33:36 +0900

    feat: 내 아티스트 구독 내역 상세 조회

--- BE/ari/src/main/java/com/ccc/ari/global/composition/service/mypage/MyArtisSubscriptionService.java
+++ BE/ari/src/main/java/com/ccc/ari/global/composition/service/mypage/MyArtisSubscriptionService.java
@@ -1,17 +1,24 @@
 package com.ccc.ari.global.composition.service.mypage;
 
+import com.ccc.ari.aggregation.ui.client.StreamingCountClient;
+import com.ccc.ari.aggregation.ui.response.GetArtistTrackCountListResponse;
+import com.ccc.ari.aggregation.ui.response.TrackCountResult;
+import com.ccc.ari.global.composition.response.mypage.GetMyArtistSubscriptionDetailResponse;
 import com.ccc.ari.global.composition.response.mypage.GetMyArtistSubscriptionResponse;
 import com.ccc.ari.global.error.ApiException;
 import com.ccc.ari.global.error.ErrorCode;
 import com.ccc.ari.global.type.PlanType;
 import com.ccc.ari.member.domain.client.MemberClient;
 import com.ccc.ari.subscription.domain.Subscription;
+import com.ccc.ari.subscription.domain.SubscriptionCycle;
 import com.ccc.ari.subscription.domain.SubscriptionPlan;
 import com.ccc.ari.subscription.domain.client.SubscriptionClient;
+import com.ccc.ari.subscription.domain.client.SubscriptionCycleClient;
 import com.ccc.ari.subscription.domain.client.SubscriptionPlanClient;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.math.BigDecimal;
 import java.util.List;
 
 /*
@@ -24,7 +31,10 @@ public class MyArtisSubscriptionService {
     private final SubscriptionPlanClient subscriptionPlanClient;
     private final SubscriptionClient subscriptionClient;
     private final MemberClient memberClient;
+    private final SubscriptionCycleClient subscriptionCycleClient;
+    private final StreamingCountClient streamingCountClient;
 
+    // 나의 아티스트 구독 목록 조회
     public GetMyArtistSubscriptionResponse getMyArtistSubscription(Integer memberId){
 
         // 내 구독 목록 조회
@@ -57,6 +67,58 @@ public class MyArtisSubscriptionService {
 
         return GetMyArtistSubscriptionResponse.builder()
                 .artists(artistSubscriptions)
+                .build();
+    }
+
+    // 내 아티스트 구독 상세 조회
+
+    public GetMyArtistSubscriptionDetailResponse getMyArtistSubscriptionDetail(Integer memberId,Integer artistId){
+
+        // 내 구독 목록 조회
+        List<Subscription> mySubscriptionIdList = subscriptionClient.getSubscriptionInfo(memberId)
+                .orElseThrow(()-> new ApiException(ErrorCode.SUBSCRIPTION_NOT_FOUND));
+
+        GetArtistTrackCountListResponse trackCountListResponse = streamingCountClient.getArtistTrackCounts(artistId);
+
+        // 전체 스트리밍 횟수 계산
+        int totalStreamingCount = trackCountListResponse.getTrackCountList().stream()
+                .mapToInt(TrackCountResult::getTotalCount)
+                .sum();
+
+        // 내 아티스트 구독 플랜 조회
+        List<GetMyArtistSubscriptionDetailResponse.ArtistSubscriptionDetail> artistSubscriptions =
+                mySubscriptionIdList.stream()
+                        .filter(subscription -> {
+                            SubscriptionPlan plan =
+                                    subscriptionPlanClient.getSubscriptionPlan(subscription.getSubscriptionPlanId());
+                            return artistId.equals(plan.getArtistId());
+                        })
+                        .flatMap(subscription -> {
+
+                            SubscriptionPlan plan =
+                                    subscriptionPlanClient.getSubscriptionPlan(subscription.getSubscriptionPlanId());
+                            List<SubscriptionCycle> cycles = subscriptionCycleClient.getSubscriptionCycle(subscription.getSubscriptionId());
+
+                            return cycles.stream()
+                                    .map(cycle -> GetMyArtistSubscriptionDetailResponse.ArtistSubscriptionDetail.builder()
+                                            .planType(plan.getPlanType().name())
+                                            .startedAt(cycle.getStartedAt())
+                                            .endedAt(cycle.getEndedAt())
+                                            .settlement(BigDecimal.valueOf(11.11)) // plan에 금액 필드 있다고 가정
+                                            .build());
+                        })
+                        .toList();
+
+
+
+
+
+        return GetMyArtistSubscriptionDetailResponse.builder()
+                .artistNickName(memberClient.getNicknameByMemberId(artistId))
+                .profileImageUrl(memberClient.getProfileImageUrlByMemberId(artistId))
+                .totalStreamingCount(totalStreamingCount)
+                .totalSettlement(BigDecimal.valueOf(11.11))
+                .subscriptions(artistSubscriptions)
                 .build();
     }
 
