34156d6d4a6c583c75e5c62f8f89f359346f818f
김준석 <crush0047@gmail.com>
2025-04-06 20:56:45 +0900

    feat: 응답 객체의 시간 데이터 Json 포맷 추가

--- BE/ari/src/main/java/com/ccc/ari/subscription/application/response/GetMyRegularCyclesResponse.java
+++ BE/ari/src/main/java/com/ccc/ari/subscription/application/response/GetMyRegularCyclesResponse.java
@@ -1,5 +1,6 @@
 package com.ccc.ari.subscription.application.response;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.*;
 
 import java.time.LocalDateTime;
@@ -10,6 +11,10 @@ import java.time.LocalDateTime;
 public class GetMyRegularCyclesResponse {
 
     private int cycleId;
+
+    @JsonFormat(pattern = "yyyy.MM.dd HH:mm:ss")
     private LocalDateTime startedAt;
+
+    @JsonFormat(pattern = "yyyy.MM.dd HH:mm:ss")
     private LocalDateTime endedAt;
 }
