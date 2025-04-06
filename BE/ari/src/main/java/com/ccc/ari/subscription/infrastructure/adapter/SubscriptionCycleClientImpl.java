b9f7b1d12fe02caca376846897380fc72836d62d
kyuhyeon <wjdrbgus8167@naver.com>
2025-04-07 00:34:00 +0900

    feat: 구독 사이클 Client 구현

---
+++ BE/ari/src/main/java/com/ccc/ari/subscription/infrastructure/adapter/SubscriptionCycleClientImpl.java
@@ -0,0 +1,24 @@
+package com.ccc.ari.subscription.infrastructure.adapter;
+
+import com.ccc.ari.subscription.domain.SubscriptionCycle;
+import com.ccc.ari.subscription.domain.client.SubscriptionCycleClient;
+import com.ccc.ari.subscription.domain.repository.SubscriptionCycleRepository;
+import com.ccc.ari.subscription.domain.vo.SubscriptionId;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+@Component
+@RequiredArgsConstructor
+public class SubscriptionCycleClientImpl implements SubscriptionCycleClient {
+
+    private final SubscriptionCycleRepository subscriptionCycleRepository;
+
+
+    public List<SubscriptionCycle> getSubscriptionCycle(SubscriptionId subscriptionId) {
+
+        List<SubscriptionCycle> cycle = subscriptionCycleRepository.getSubscriptionCycleList(subscriptionId) ;
+        return cycle;
+    }
+}
