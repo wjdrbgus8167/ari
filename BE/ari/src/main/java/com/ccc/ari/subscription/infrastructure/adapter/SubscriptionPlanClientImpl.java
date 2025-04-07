9b17f5db383826029aa2fb6ba32ddf5495437445
강지민 <kjmemail2743@gmail.com>
2025-04-08 00:58:24 +0900

    feat: 아티스트의 구독권 조회를 위한 메서드 추가

--- BE/ari/src/main/java/com/ccc/ari/subscription/infrastructure/adapter/SubscriptionPlanClientImpl.java
+++ BE/ari/src/main/java/com/ccc/ari/subscription/infrastructure/adapter/SubscriptionPlanClientImpl.java
@@ -6,15 +6,21 @@ import com.ccc.ari.subscription.domain.repository.SubscriptionPlanRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 
+import java.util.Optional;
+
 @Component
 @RequiredArgsConstructor
 public class SubscriptionPlanClientImpl implements SubscriptionPlanClient {
 
     private final SubscriptionPlanRepository subscriptionPlanRepository;
 
-
     @Override
     public SubscriptionPlan getSubscriptionPlan(Integer subscriptionPlanId) {
         return subscriptionPlanRepository.findSubscriptionPlanBySubscriptionPlanId(subscriptionPlanId);
+    }
+
+    @Override
+    public Optional<SubscriptionPlan> getSubscriptionPlanByArtistId(Integer artistId) {
+        return subscriptionPlanRepository.findSubscriptionPlanByArtistId(artistId);
     }
 }
