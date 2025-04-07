c35761574734d3a836a359a95b07b7adcc390d85
강지민 <kjmemail2743@gmail.com>
2025-04-08 01:29:19 +0900

    feat: 팬톡 목록 조회 응답에 구독 여부 추가

--- BE/ari/src/main/java/com/ccc/ari/global/composition/controller/community/FantalkCompositionController.java
+++ BE/ari/src/main/java/com/ccc/ari/global/composition/controller/community/FantalkCompositionController.java
@@ -2,8 +2,10 @@ package com.ccc.ari.global.composition.controller.community;
 
 import com.ccc.ari.global.composition.response.community.FantalkListResponse;
 import com.ccc.ari.global.composition.service.community.FantalkListService;
+import com.ccc.ari.global.security.MemberUserDetails;
 import com.ccc.ari.global.util.ApiUtils;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -20,10 +22,13 @@ public class FantalkCompositionController {
     private final FantalkListService fantalkListService;
 
     @GetMapping
-    public ApiUtils.ApiResponse<FantalkListResponse> getFantalkList(@PathVariable Integer fantalkChannelId) {
+    public ApiUtils.ApiResponse<FantalkListResponse> getFantalkList(
+            @PathVariable Integer fantalkChannelId,
+            @AuthenticationPrincipal MemberUserDetails userDetails
+    ) {
 
-        FantalkListResponse response = fantalkListService.getFantalkList(fantalkChannelId);
-
+        Integer memberId = userDetails.getMemberId();;
+        FantalkListResponse response = fantalkListService.getFantalkList(fantalkChannelId, memberId);
         return ApiUtils.success(response);
     }
 }
