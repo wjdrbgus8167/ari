217d5edc485e504a17dc796b886d6ce1251629a6
강지민 <kjmemail2743@gmail.com>
2025-04-08 00:57:52 +0900

    feat: 구독자 수 카운트를 위한 메서드 추가

--- BE/ari/src/main/java/com/ccc/ari/subscription/domain/client/SubscriptionClient.java
+++ BE/ari/src/main/java/com/ccc/ari/subscription/domain/client/SubscriptionClient.java
@@ -9,4 +9,10 @@ public interface SubscriptionClient {
 
     // 사용자 ID로 구독 정보 가여오기
     Optional<List<Subscription>> getSubscriptionInfo(Integer memberId);
+
+    // 구독자인지 확인
+    Boolean hasActiveSubscription(Integer memberId, Integer subscriptionPlanId);
+
+    // 구독자 수 카운트
+    Integer countActiveSubscribersByPlanId(Integer subscriptionPlanId);
 }
