c44d86c8a3fc82e81634dc5117db57bf1b857caf
김준석 <crush0047@gmail.com>
2025-04-08 11:44:52 +0900

    feat: 아티스트 대시보드 구성을 위한 정산 데이터 조회 기능 구현

---
+++ BE/ari/src/main/java/com/ccc/ari/settlement/application/response/GetArtistDailySettlementResponse.java
@@ -0,0 +1,16 @@
+package com.ccc.ari.settlement.application.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+
+import java.util.List;
+
+@Data
+@AllArgsConstructor
+@Builder
+public class GetArtistDailySettlementResponse {
+    private final Double todaySettlement;
+    private final Double settlementDiff;
+    private final List<DailySettlementInfo> dailySettlements;
+}
