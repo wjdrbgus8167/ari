27eb085c05d2642657c20753637597196e977027
권남희 <knaem.edu@gmail.com>
2025-04-09 09:46:48 +0900

    feat: 나의 음악 서랍 - 구독 중인 아티스트 datasource

---
+++ FE/lib/data/datasources/music_drawer/subscribed_artists_remote_datasource.dart
@@ -0,0 +1,31 @@
+import 'package:ari/data/datasources/api_client.dart';
+import 'package:ari/data/models/music_drawer/subscribed_artist_model.dart';
+
+/// 나의 음악 서랍 - 구독 중인 아티스트 관련 원격 데이터소스 인터페이스
+abstract class SubscribedArtistsRemoteDataSource {
+  /// 구독 중인 아티스트 목록 조회
+  Future<SubscribedArtistsResponse> getSubscribedArtists();
+}
+
+class SubscribedArtistsRemoteDataSourceImpl
+    implements SubscribedArtistsRemoteDataSource {
+  final ApiClient _apiClient;
+
+  SubscribedArtistsRemoteDataSourceImpl(this._apiClient);
+
+  @override
+  Future<SubscribedArtistsResponse> getSubscribedArtists() async {
+    try {
+      // API 호출
+      final response = await _apiClient.get(
+        '/api/v1/mypages/subscriptions/artists/list',
+      );
+
+      // 응답 파싱
+      return SubscribedArtistsResponse.fromJson(response);
+    } catch (e) {
+      // 에러 처리
+      rethrow;
+    }
+  }
+}
