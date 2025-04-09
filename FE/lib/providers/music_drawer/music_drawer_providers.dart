80e425648e83b095a216f01bd1947c1f450e0d06
권남희 <knaem.edu@gmail.com>
2025-04-09 09:57:17 +0900

    feat: 나의 음악 서랍 - provider 구현

---
+++ FE/lib/providers/music_drawer/music_drawer_providers.dart
@@ -0,0 +1,68 @@
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:dio/dio.dart';
+import 'package:ari/data/datasources/api_client.dart';
+import 'package:ari/data/datasources/music_drawer/subscribed_artists_remote_datasource.dart';
+import 'package:ari/data/repositories/music_drawer/subscribed_artists_repository_impl.dart';
+import 'package:ari/domain/repositories/music_drawer/subscribed_artists_repository.dart';
+import 'package:ari/domain/usecases/music_drawer/subscribed_artists_usecases.dart';
+import 'package:ari/presentation/viewmodels/music_drawer/subscribed_artists_viewmodel.dart';
+
+final dioProvider = Provider<Dio>((ref) {
+  final dio = Dio();
+  dio.options.baseUrl = 'https://ari-music.duckdns.org'; // 서버 URL 설정
+  dio.options.connectTimeout = const Duration(seconds: 10);
+  dio.options.receiveTimeout = const Duration(seconds: 10);
+  return dio;
+});
+
+/// API 클라이언트 프로바이더
+final apiClientProvider = Provider<ApiClient>((ref) {
+  final dio = ref.watch(dioProvider);
+  return ApiClient(dio);
+});
+
+/// 구독 중인 아티스트 데이터소스 프로바이더
+final subscribedArtistsDataSourceProvider =
+    Provider<SubscribedArtistsRemoteDataSource>((ref) {
+      final apiClient = ref.watch(apiClientProvider);
+      return SubscribedArtistsRemoteDataSourceImpl(apiClient);
+    });
+
+/// 구독 중인 아티스트 레포지토리 프로바이더
+final subscribedArtistsRepositoryProvider =
+    Provider<SubscribedArtistsRepository>((ref) {
+      final dataSource = ref.watch(subscribedArtistsDataSourceProvider);
+      return SubscribedArtistsRepositoryImpl(dataSource);
+    });
+
+/// 구독 중인 아티스트 목록 조회 유스케이스 프로바이더
+final getSubscribedArtistsUseCaseProvider =
+    Provider<GetSubscribedArtistsUseCase>((ref) {
+      final repository = ref.watch(subscribedArtistsRepositoryProvider);
+      return GetSubscribedArtistsUseCase(repository);
+    });
+
+/// 구독 중인 아티스트 수 조회 유스케이스 프로바이더
+final getSubscribedArtistsCountUseCaseProvider =
+    Provider<GetSubscribedArtistsCountUseCase>((ref) {
+      final repository = ref.watch(subscribedArtistsRepositoryProvider);
+      return GetSubscribedArtistsCountUseCase(repository);
+    });
+
+/// 구독 중인 아티스트 뷰모델 프로바이더
+final subscribedArtistsViewModelProvider = StateNotifierProvider<
+  SubscribedArtistsViewModel,
+  SubscribedArtistsState
+>((ref) {
+  final getArtistsUseCase = ref.watch(getSubscribedArtistsUseCaseProvider);
+  final getArtistsCountUseCase = ref.watch(
+    getSubscribedArtistsCountUseCaseProvider,
+  );
+  return SubscribedArtistsViewModel(getArtistsUseCase, getArtistsCountUseCase);
+});
+
+/// 구독 중인 아티스트 수 프로바이더
+final subscribedArtistsCountProvider = FutureProvider<int>((ref) async {
+  final viewModel = ref.watch(subscribedArtistsViewModelProvider.notifier);
+  return await viewModel.getSubscribedArtistsCount();
+});
